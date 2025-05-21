bc3571948d84432355a1d45c5f3e0765f68d6d93
sugichan-a <sugihara200303@gmail.com>
2025-05-21 11:31:43 +0900

    環境構築

---
+++ backend/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Express!');
+});
+
+app.listen(3000, () => {
+  console.log('サーバーが port 3000 で動いています');
+});
